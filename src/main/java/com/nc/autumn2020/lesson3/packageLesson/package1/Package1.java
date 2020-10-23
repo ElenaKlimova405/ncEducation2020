b5b4254710151ad8246b5b4c59b17d2ec67a965b
ElenaKlimova405 <klimovaev405@mail.ru>
2020-10-23 18:04:50 +0400

    git push https://github.com/ElenaKlimova405/ncEducation2020.git

---
+++ src/main/java/com/nc/autumn2020/lesson3/packageLesson/package1/Package1.java
@@ -0,0 +1,4 @@
+package com.nc.autumn2020.lesson3.packageLesson.package1;
+
+public class Package1 {
+}
---
+++ src/main/java/com/nc/autumn2020/lesson3/packageLesson/package1/StaticImportConstants.java
@@ -0,0 +1,5 @@
+package com.nc.autumn2020.lesson3.packageLesson.package1;
+
+public class StaticImportConstants {
+    public  static final String SOME_CONSTANT = "constant";
+}
